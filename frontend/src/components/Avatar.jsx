9431a5a7c0ea82ddd262fce343869f8e8e5c4dec
Hacker Gaming <hg7095947@gmail.com>
2025-05-23 06:56:22 +0000

    some major bug fixed

--- frontend/src/components/Avatar.jsx
+++ frontend/src/components/Avatar.jsx
@@ -1,30 +1,40 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { toast } from 'react-hot-toast';
+import { toast } from "react-hot-toast";
 
 const Avatar = () => {
   const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
   const [image, setImage] = useState(null);
-  const [user, setUser] = useState({})
-  const [file, setFile] = useState(null)
+  const [submitting, setSubmitting] = useState(false);
+  const [user, setUser] = useState({});
+  const [file, setFile] = useState(null);
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
+
+    const maxSize = 2 * 1024 * 1024; // 2MB
+
+    if (file.size > maxSize) {
+      return toast.error("Image size should be less then 2MB")
+    }
+    
     if (file) {
       const imageUrl = URL.createObjectURL(file);
-      setImage(imageUrl)
+      setImage(imageUrl);
       setFile(file);
     }
   };
 
   const handleSubmit = async (e) => {
+    setSubmitting(true)
     e.preventDefault();
 
     if (!file) {
-      return toast.error("Please select an image")
+      setSubmitting(false)
+      return toast.error("Please select an image");
     }
-    
+
     const formData = new FormData();
     formData.append("avatar", file);
     try {
@@ -35,35 +45,40 @@ const Avatar = () => {
           withCredentials: true,
         },
       );
-      if (response.status === 200){
-        return toast.success("Avatar uploaded successfully"); 
+      if (response.status === 200) {
+        setSubmitting(false)
+        return toast.success("Avatar uploaded successfully");
       }
     } catch (error) {
+      setSubmitting(false)
       return toast.error(error.message);
     }
   };
 
-  useEffect(() =>{
+  useEffect(() => {
     const getUser = async () => {
       try {
-        const response = await axios.get(`${baseUrl}/api/v1/user/current-user`, {
-      withCredentials: true
-    })
+        const response = await axios.get(
+          `${baseUrl}/api/v1/user/current-user`,
+          {
+            withCredentials: true,
+          },
+        );
         if (response.status === 200) {
-          setUser(response?.data?.user)
+          setUser(response?.data?.user);
         }
       } catch (error) {
-        return toast.error(error?.response?.data?.message)
+        return toast.error(error?.response?.data?.message);
       }
-    }
-    getUser()
-  }, [])
+    };
+    getUser();
+  }, []);
 
   useEffect(() => {
     if (user.avatar) {
-      setImage(user.avatar)
+      setImage(user.avatar);
     }
-  }, [user])
+  }, [user]);
 
   return (
     <form onSubmit={handleSubmit}>
@@ -80,15 +95,17 @@ const Avatar = () => {
         <input
           type="file"
           accept="image/*"
+          name="avatar"
           onChange={handleFileChange}
           className="hidden"
         />
       </label>
       <button
         type="submit"
-        className="mt-2 flex justify-center items-center gap-4 text-white text-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 outline-none font-medium rounded-lg text-sm px-4 font-bold py-3 text-center"
+        disabled={submitting}
+        className="mt-2 flex justify-center items-center gap-4 text-white text-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 outline-none font-medium rounded-lg text-sm px-4 font-bold py-3 text-center hover:bg-blue-700 disabled:bg-blue-500"
       >
-        Upload Avatar
+        {submitting ? "Uploading..." : "Upload Avatar"}
       </button>
     </form>
   );
